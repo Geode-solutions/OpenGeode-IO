59319447300f57e5791777e7c48363ebf2cd5b9d
Azure Pipelines <azure@pipelines.com>
2019-10-02 13:49:48 +0000

    style: CI format update

--- src/geode/mesh/detail/assimp_input.cpp
+++ src/geode/mesh/detail/assimp_input.cpp
@@ -34,8 +34,9 @@ namespace geode
         bool AssimpMeshInput::read_file()
         {
             const auto* pScene = importer_.ReadFile( file_.c_str(), 0 );
-            OPENGEODE_EXCEPTION( pScene, 
-                "[AssimpMeshInput::read_file]" + std::string{ importer_.GetErrorString() } );
+            OPENGEODE_EXCEPTION(
+                pScene, "[AssimpMeshInput::read_file]"
+                            + std::string{ importer_.GetErrorString() } );
             OPENGEODE_EXCEPTION( pScene->mNumMeshes == 1,
                 "[AssimpMeshInput::read_file] Several meshes in imported file "
                     + file_ + "." );
