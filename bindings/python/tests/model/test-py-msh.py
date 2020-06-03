5b5e714a1ea5968c33c12cb27d855199314385c9
panquez <pierre.anquez@geode-solutions.com>
2020-06-03 16:10:08 +0200

    add python test

--- bindings/python/tests/model/test-py-msh.py
+++ bindings/python/tests/model/test-py-msh.py
@@ -27,7 +27,7 @@ import opengeode_py_mesh as mesh
 import opengeode_py_model as model
 import opengeode_io_py_model as model_io
 
-def test_brep(brep):
+def test_brep_cube(brep):
     # Number of components
     if brep.nb_corners() != 8:
         raise ValueError("[Test] Number of corners is not correct" )
@@ -80,16 +80,77 @@ def test_brep(brep):
         if brep.nb_incidences( b.id() ) != 0:
             raise ValueError("[Test] Number of block incidences should be 0" )
 
+def test_brep_cone(brep):
+    # Number of components
+    if brep.nb_corners() != 6:
+        raise ValueError("[Test] Number of corners is not correct")
+    if brep.nb_lines() != 13:
+        raise ValueError("[Test] Number of lines is not correct")
+    if brep.nb_surfaces() != 12:
+        raise ValueError("[Test] Number of surfaces is not correct")
+    if brep.nb_blocks() != 4:
+        raise ValueError("[Test] Number of blocks is not correct")
+
+    # Number of vertices and elements in components
+    for c in brep.corners():
+        if c.mesh().nb_vertices() != 1:
+            raise ValueError("[Test] Number of vertices in corners should be 1")
+    for l in brep.lines():
+        if l.mesh().nb_vertices() == 0:
+            raise ValueError("[Test] Number of vertices in lines should not be null")
+        if l.mesh().nb_edges() == 0:
+            raise ValueError("[Test] Number of edges in lines should not be null")
+    for s in brep.surfaces():
+        if s.mesh().nb_vertices() == 0:
+            raise ValueError("[Test] Number of vertices in surfaces should not be null")
+        if s.mesh().nb_polygons() == 0:
+            raise ValueError("[Test] Number of polygons in surfaces should not be null")
+    for b in brep.blocks():
+        if b.mesh().nb_vertices() == 0:
+            raise ValueError("[Test] Number of vertices in blocks should not be null")
+        if b.mesh().nb_polyhedra() == 0:
+            raise ValueError("[Test] Number of polyhedra in blocks should not be null")
+
+    # Number of component boundaries and incidences
+    for c in brep.corners():
+        if brep.nb_boundaries( c.id() ) != 0:
+            raise ValueError("[Test] Number of corner boundary should be 0" )
+        if brep.nb_incidences( c.id() ) != 4 and brep.nb_incidences( c.id() ) != 5:
+            raise ValueError("[Test] Number of corner incidences should be 4 or 5" )
+    for l in brep.lines():
+        if brep.nb_boundaries( l.id() ) != 2:
+            raise ValueError("[Test] Number of line boundary should be 2" )
+        if brep.nb_incidences( l.id() ) < 2 or brep.nb_incidences( l.id() ) > 4:
+            raise ValueError("[Test] Number of line incidences should be 2, 3 or 4" )
+    for s in brep.surfaces():
+        if brep.nb_boundaries( s.id() ) != 3:
+            raise ValueError("[Test] Number of surface boundary should be 3" )
+        if brep.nb_incidences( s.id() ) != 1 and brep.nb_incidences( s.id() ) != 2:
+            raise ValueError("[Test] Number of surface incidences should be 1 or 2" )
+    for b in brep.blocks():
+        if brep.nb_boundaries( b.id() ) != 4:
+            raise ValueError("[Test] Number of block boundary should be 4" )
+        if brep.nb_incidences( b.id() ) != 0:
+            raise ValueError("[Test] Number of block incidences should be 0" )
+
+
 if __name__ == '__main__':
     model_io.initialize_model_io()
     test_dir = os.path.dirname(__file__)
     data_dir = os.path.abspath(os.path.join(test_dir, "../../../../tests/data"))
 
-    brep = model.BRep()
-    model.load_brep(brep, os.path.join(data_dir, "cube_v22.msh" ))
-    test_brep(brep)
+    brep_cube = model.BRep()
+    model.load_brep(brep_cube, os.path.join(data_dir, "cube_v22.msh" ))
+    test_brep_cube(brep_cube)
+    model.save_brep(brep_cube, "cube_v22.og_brep")
+    reloaded_brep_cube = model.BRep()
+    model.load_brep(reloaded_brep_cube, "cube_v22.og_brep")
+    test_brep_cube(reloaded_brep_cube)
 
-    model.save_brep( brep, "cube_v22.og_brep")
-    reloaded_brep = model.BRep()
-    model.load_brep(reloaded_brep, "cube_v22.og_brep")
-    test_brep(reloaded_brep)
+    brep_cone = model.BRep()
+    model.load_brep(brep_cone, os.path.join(data_dir, "cone_v4.msh" ))
+    test_brep_cone(brep_cone)
+    model.save_brep(brep_cone, "cone_v4.og_brep")
+    reloaded_brep_cone = model.BRep()
+    model.load_brep(reloaded_brep_cone, "cone_v4.og_brep")
+    test_brep_cone(reloaded_brep_cone)
