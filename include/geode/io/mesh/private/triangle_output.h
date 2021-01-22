593c183c4ef9aa58e4eb116b86d4c87b3de87797
BotellaA <arnaud.botella@geode-solutions.com>
2021-01-22 13:50:30 +0100

    feat(TriangleOutput): add triangle output (node, ele, neigh)

---
+++ include/geode/io/mesh/private/triangle_output.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2019 - 2021 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#pragma once
+
+#include <geode/mesh/io/triangulated_surface_output.h>
+
+namespace geode
+{
+    namespace detail
+    {
+        class TriangleOutput final : public TriangulatedSurfaceOutput< 2 >
+        {
+        public:
+            TriangleOutput( const TriangulatedSurface< 2 > &surface,
+                absl::string_view filename )
+                : TriangulatedSurfaceOutput< 2 >( surface, filename )
+            {
+            }
+
+            static absl::string_view extension()
+            {
+                static constexpr auto ext = "triangle";
+                return ext;
+            }
+
+            void write() const final;
+        };
+    } // namespace detail
+} // namespace geode
---
+++ src/geode/io/mesh/triangle_output.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2019 - 2021 Geode-solutions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <geode/io/mesh/private/triangle_output.h>
+
+#include <fstream>
+
+#include <geode/geometry/point.h>
+
+#include <geode/mesh/core/triangulated_surface.h>
+
+namespace
+{
+    static constexpr char EOL{ '\n' };
+    static constexpr char SPACE{ ' ' };
+
+    void write_node(
+        const std::string& filename, const geode::TriangulatedSurface2D& mesh )
+    {
+        std::ofstream node{ filename };
+        node << mesh.nb_vertices() << " 2 0 0" << EOL;
+        for( const auto v : geode::Range{ mesh.nb_vertices() } )
+        {
+            node << v << SPACE << mesh.point( v ).string() << EOL;
+        }
+        node << std::flush;
+    }
+
+    void write_ele(
+        const std::string& filename, const geode::TriangulatedSurface2D& mesh )
+    {
+        std::ofstream ele{ filename };
+        ele << mesh.nb_polygons() << " 3 0" << EOL;
+        for( const auto p : geode::Range{ mesh.nb_polygons() } )
+        {
+            ele << p;
+            for( const auto v : geode::LRange{ 3 } )
+            {
+                ele << SPACE << mesh.polygon_vertex( { p, v } );
+            }
+            ele << EOL;
+        }
+        ele << std::flush;
+    }
+
+    void write_neigh(
+        const std::string& filename, const geode::TriangulatedSurface2D& mesh )
+    {
+        std::ofstream neigh{ filename };
+        neigh << mesh.nb_polygons() << " 3" << EOL;
+        for( const auto p : geode::Range{ mesh.nb_polygons() } )
+        {
+            neigh << p;
+            for( const auto e : geode::LRange{ 3 } )
+            {
+                neigh << SPACE;
+                if( const auto adj = mesh.polygon_adjacent( { p, e } ) )
+                {
+                    neigh << adj.value();
+                }
+                else
+                {
+                    neigh << -1;
+                }
+            }
+            neigh << EOL;
+        }
+        neigh << std::flush;
+    }
+} // namespace
+
+namespace geode
+{
+    namespace detail
+    {
+        void TriangleOutput::write() const
+        {
+            auto file = filename();
+            file.remove_suffix( TriangleOutput::extension().size() + 1 );
+            const auto& mesh = triangulated_surface();
+            write_node( absl::StrCat( file, ".node" ), mesh );
+            write_ele( absl::StrCat( file, ".ele" ), mesh );
+            write_neigh( absl::StrCat( file, ".neigh" ), mesh );
+        }
+    } // namespace detail
+} // namespace geode
